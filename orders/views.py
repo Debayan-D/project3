a27150da7747a83a2bb53d4824e8d5c917f88e24
ddey123 <debayan1206@gmail.com>
2020-06-16 13:37:52 +0100

    Committed files

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls import reverse
+from django.db.models import Sum
+from .models import Category,Regular_pizza,Sicilian_pizza,Topping,Sub,Pasta,Salad,Dinner_platter,Order2,User_order,Order_counter
 
 # Create your views here.
+
 def index(request):
     return HttpResponse("Project 3: TODO")
