3fa17430583c24481839f7f7ab0f9cb92d53ebcc
ddey123 <debayan1206@gmail.com>
2020-06-18 20:02:03 +0100

    Changed name of Order2

--- orders/views.py
+++ orders/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse
 from django.db.models import Sum
-from .models import Category,Regular_pizza,Sicilian_pizza,Topping,Sub,Pasta,Salad,Dinner_platter,Order2,User_order,Order_counter
+from .models import Category,Regular_pizza,Sicilian_pizza,Topping,Sub,Pasta,Salad,Dinner_platter,Order,User_order,Order_counter
 
 # Create your views here.
 
